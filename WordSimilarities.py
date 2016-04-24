655c77320dfbc746f16bfd8ea9f176aea8e6ee8b
Johannes Gontrum <gontrum@uni-potsdam.de>
2016-04-24 22:58:56 +0200

    Structure for the evaluation class

---
+++ WordSimilarities.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+
+class WordSimilarities(object):
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def get_vector_for_word(self):
+        return
+
+    def get_vector_for_id(self):
+        return
+
+    def get_id_for_word(self):
+        return
+
+    def get_word_for_id(self):
+        return
+
+    def get_similarity(self):
+        return
+
+    def get_most_similar_word(self):
+        return
+
+    def __read_evaluation_words(self):
+        return
+
+    def __read_signature(self):
+        return
+
+    def __read_weights(self):
+        return
+
+if __name__ == '__main__':
+    sim = WordSimilarities('./')
